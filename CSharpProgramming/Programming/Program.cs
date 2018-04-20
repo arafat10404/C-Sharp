e8374e143231afabb9c0e04555f26f004371c8b2
Redwan Hossain <redwanh25@gmail.com>
2018-04-21 02:52:15 +0600

    Add files via upload

---
+++ CSharpProgramming/Programming/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming
+{
+    abstract class Foo
+    {
+        public abstract void M();
+    }
+
+    class Bar : Foo
+    {
+        public override void M()
+        {
+            Console.WriteLine("Bar.M");
+        }
+    }
+    class Example
+    {
+        static void Main()
+        {
+            try
+            {
+                int[] arr = new int[5];
+                arr[10] = 25;
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine(e.GetType());
+            }
+
+            Foo b = new Bar();
+            b.M();
+        }
+    }
+
+}
